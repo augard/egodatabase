1813265c4da7d5e5e328c480934e6872f0ab497d
Lukas Foldyna <lukas.foldyna@arat.cz>
2011-11-20 12:12:44 +0100

    Replaced NSAutoreleasePool with @autoreleasepool

--- EGODatabaseRequest.m
+++ EGODatabaseRequest.m
@@ -62,50 +62,50 @@
 	[self main];
 }
 - (void)main {
-	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-	[delegate retain];
-	
-	if(self.requestKind == EGODatabaseUpdateRequest) {
-		BOOL result = [self.database executeUpdate:query parameters:parameters];
-		
-		if(result) {
-			[self didSucceedWithResult:nil];
-		} else {
-			NSString* errorMessage = [self.database lastErrorMessage];
-			NSDictionary* userInfo = nil;
-			
-			if(errorMessage) {
-				userInfo = [NSDictionary dictionaryWithObject:errorMessage forKey:@"message"];
-			}
-			
-			NSError* error = [NSError errorWithDomain:@"com.egodatabase.update"
-												 code:[self.database lastErrorCode]
-											 userInfo:userInfo];
-			
-			[self didFailWithError:error];
-		}
-	} else {
-		EGODatabaseResult* result = [self.database executeQuery:query parameters:parameters];
-		
-		if(result.errorCode == 0) {
-			[self didSucceedWithResult:result];
-		} else {
-			NSDictionary* userInfo = nil;
-			
-			if(result.errorMessage) {
-				userInfo = [NSDictionary dictionaryWithObject:result.errorMessage forKey:@"message"];
-			}
-			
-			NSError* error = [NSError errorWithDomain:@"com.egodatabase.select"
-												 code:result.errorCode
-											 userInfo:userInfo];
-			
-			[self didFailWithError:error];
-		}
-	}
-	
-	[delegate release];
-	[pool release];
+	@autoreleasepool {
+        [delegate retain];
+        
+        if(self.requestKind == EGODatabaseUpdateRequest) {
+            BOOL result = [self.database executeUpdate:query parameters:parameters];
+            
+            if(result) {
+                [self didSucceedWithResult:nil];
+            } else {
+                NSString* errorMessage = [self.database lastErrorMessage];
+                NSDictionary* userInfo = nil;
+                
+                if(errorMessage) {
+                    userInfo = [NSDictionary dictionaryWithObject:errorMessage forKey:@"message"];
+                }
+                
+                NSError* error = [NSError errorWithDomain:@"com.egodatabase.update"
+                                                     code:[self.database lastErrorCode]
+                                                 userInfo:userInfo];
+                
+                [self didFailWithError:error];
+            }
+        } else {
+            EGODatabaseResult* result = [self.database executeQuery:query parameters:parameters];
+            
+            if(result.errorCode == 0) {
+                [self didSucceedWithResult:result];
+            } else {
+                NSDictionary* userInfo = nil;
+                
+                if(result.errorMessage) {
+                    userInfo = [NSDictionary dictionaryWithObject:result.errorMessage forKey:@"message"];
+                }
+                
+                NSError* error = [NSError errorWithDomain:@"com.egodatabase.select"
+                                                     code:result.errorCode
+                                                 userInfo:userInfo];
+                
+                [self didFailWithError:error];
+            }
+        }
+        
+        [delegate release];
+    }
 }
 
 - (void)didSucceedWithResult:(EGODatabaseResult*)result {
